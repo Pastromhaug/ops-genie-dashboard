a6e8f06ce00bacef9799b64991e473ceadfea789
Peter Stromhaug <pas282@cornell.edu>
2016-06-03 14:54:42 +0200

    working post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/', function(req, res) {
+    console.log('post received');
+    console.log(req.body);
+    res.send('hi there');
+});
 
 app.get('/', function (req, res) {
     console.log(__dirname + '/public/index.html');
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
+app.listen(8000, '10.40.104.111', function () {
     console.log(__dirname + "/public/bundle.js");
     console.log('Example app listening on port 3000!');
 });
+
+
+//function init() {
+//
+//}
