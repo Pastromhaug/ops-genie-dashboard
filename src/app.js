c25fce803b110c05ba6e3b4d6530f8a31294c45a
Peter Stromhaug <pas282@cornell.edu>
2016-06-05 13:40:11 +0200

    basic socket.io example working

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require('path');
 var bodyParser = require('body-parser');
 //var $ = require('jquery');
 
@@ -7,6 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+    socket.emit('server event', { foo: 'bar' });
+    socket.on('client event', function (data) {
+        console.log(data);
+    });
+});
+
 
 
 data = {};
@@ -31,7 +42,7 @@ app.post('/', function(req, res) {
 
 app.get('/', function (req, res) {
     console.log(__dirname + '/public/index.html');
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/data', function(req, res) {
@@ -40,7 +51,7 @@ app.get('/data', function(req, res) {
     res.send(JSON.stringify(data));
 });
 
-app.listen(8000, function () {
+server.listen(8000, function () {
     console.log(__dirname + "/public/bundle.js");
     console.log('Example app listening on port 3000!');
 });
