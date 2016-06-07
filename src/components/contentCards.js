11eb092683d23ebbda921291bb1a7574dda1d9e8
Peter Stromhaug <pas282@cornell.edu>
2016-06-07 18:18:12 +0200

    now removes and downtime are working

--- src/components/contentCards.js
+++ src/components/contentCards.js
@@ -17,14 +17,24 @@ class ContentCards extends React.Component {
 
     constructor(props) {
         super(props);
+        this.newDowntimeOnRemove.bind(this);
+        this.newDowntimeOnAdd.bind(this);
         socket.emit('client ready', {first: 'client_ready'});
         socket.on('add alert', (data) => {
-            props.onAddAlert(data);
+            var aliases = props._state.alerts.map( (curr) => curr.alert.alias);
+            var new_alias = data.alert.alias;
+            if (aliases.indexOf(new_alias) == -1) {
+                props.onAddAlert(data);
+                var new_downtime = this.newDowntimeOnAdd(data.alert.entity, data.alert.createdAt/1000000);
+                props.onUpdateServiceDowntime(data.alert.entity, new_downtime);
+            }
         });
         socket.on('remove alert', (data) => {
             console.log('remove alert data:');
             console.log(data);
             props.onRemoveAlert(data);
+            var new_downtime = this.newDowntimeOnRemove(data.alert.entity);
+            props.onUpdateServiceDowntime(data.alert.entity, new_downtime);
 
             // loop through services and set their last available time to null if
             // they don't have any outstanding alerts. This will zero their downtime.
@@ -56,6 +66,23 @@ class ContentCards extends React.Component {
                 <VisibleAlertsTable/>
             </div>
         )
+    }
+
+    newDowntimeOnRemove(entity) {
+        var this_entity = this.props._state.alerts.filter( (curr) => curr.alert.entity == entity);
+        if (this_entity.length == 0) {
+            return null;
+        }
+        var this_service = this.props._state.services.filter( (curr) => curr.service == entity)[0];
+        return this_service.last_time_available;
+    }
+
+    newDowntimeOnAdd(entity, created_at) {
+        var this_service = this.props._state.services.filter( (curr) => curr.service == entity)[0];
+        var old_downtime = this_service.last_time_available;
+        if (old_downtime == null) return created_at;
+        else if (created_at < old_downtime ) return created_at;
+        else return old_downtime;
     }
 }
 export default ContentCards;
